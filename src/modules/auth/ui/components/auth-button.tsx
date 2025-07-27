ee3eb34d4475304d331ee61eb4864fd8632c8863
백지수 <baegjisu@baegjisuui-MacBookAir.local>
2025-07-27 12:27:41 +0900

    feat: clerk 미들웨어 추가

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { UserIcon } from "lucide-react";
 
 export const AuthButton = () => {
-  // TODO: Add different auth states
-
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
-    >
-      <UserIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+        {/* TODO: Add menu items for Studio and User Profile */}
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+          >
+            <UserIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };

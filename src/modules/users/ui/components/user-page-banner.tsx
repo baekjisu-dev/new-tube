73ff8fe1ee6a45ba463d9f02b8798f23588268cc
백지수 <baegjisu@baegjisuui-MacBookAir.local>
2025-08-22 20:35:35 +0900

    feat: 배너 업로드

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import { UserGetOneOutput } from "@/modules/users/types";
 import { useAuth } from "@clerk/nextjs";
 import { Edit2Icon } from "lucide-react";
+import { useState } from "react";
+import { BannerUploadModal } from "./banner-upload-modal";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
@@ -15,9 +17,15 @@ export const UserPageBannerSkeleton = () => {
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
   const { userId } = useAuth();
+  const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false);
 
   return (
     <div className="relative group">
+      <BannerUploadModal
+        userId={user.id}
+        open={isBannerUploadModalOpen}
+        onOpenChange={setIsBannerUploadModalOpen}
+      />
       <div
         className={cn(
           "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to gray-200 rounded-xl bg-gray-100",
@@ -34,6 +42,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
             type="button"
             size="icon"
             className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black-50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+            onClick={() => setIsBannerUploadModalOpen(true)}
           >
             <Edit2Icon className="size-4 text-white" />
           </Button>
